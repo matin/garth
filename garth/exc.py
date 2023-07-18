126f7ea5b840f0238f35a992967a6c3e482a5a5b
Matin Tamizi <mtamizi@duck.com>
2023-07-18 16:33:01 -0600

    don't need this for now

--- garth/exc.py
+++ garth/exc.py
@@ -6,8 +6,3 @@ class GarthException(Exception):
     """Base exception for all garth exceptions."""
 
     msg: str
-
-
-@dataclass
-class GarthAuthException(GarthException):
-    """Base exception for all garth authentication exceptions."""
