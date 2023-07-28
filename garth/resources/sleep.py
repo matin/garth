355d178c8a49279d6bf777245a4d5793ebcbd359
Matin Tamizi <mtamizi@duck.com>
2023-07-27 20:50:37 -0600

    fix bug

--- garth/resources/sleep.py
+++ garth/resources/sleep.py
@@ -10,7 +10,7 @@ from ..utils import camel_to_snake_dict, date_range, format_end_date
 
 @dataclass(frozen=True)
 class DailySleep(Stats):
-    calendar_date: date
+    value: int
 
     _path: ClassVar[str] = "/wellness-service/stats/daily/sleep/score"
     _page_size: ClassVar[int] = 28
