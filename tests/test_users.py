e3887d35eaaa3d0d3dd14610faf42fe98c9fe5cf
Jordan Barker <Jordanbarker5@gmail.com>
2025-04-15 20:19:31 -0500

    Add test_user_settings_sleep_windows pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -14,3 +14,9 @@ def test_user_profile(authed_client: Client):
 def test_user_settings(authed_client: Client):
     settings = UserSettings.get(client=authed_client)
     assert settings.user_data
+
+
+@pytest.mark.vcr
+def test_user_settings_sleep_windows(authed_client: Client):
+    settings = UserSettings.get(client=authed_client)
+    assert settings.user_data
