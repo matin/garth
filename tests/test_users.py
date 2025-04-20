9bdf4ce51c7a7fe97576d504dff6d0f886ee91c4
Jordan Barker <Jordanbarker5@gmail.com>
2025-04-20 07:08:44 -0500

    Add asserts for each window in user_sleep_windows

--- tests/test_users.py
+++ tests/test_users.py
@@ -20,3 +20,8 @@ def test_user_settings(authed_client: Client):
 def test_user_settings_sleep_windows(authed_client: Client):
     settings = UserSettings.get(client=authed_client)
     assert settings.user_data
+    assert isinstance(settings.user_sleep_windows, list)
+    for window in settings.user_sleep_windows:
+        assert hasattr(window, 'sleep_window_frequency')
+        assert hasattr(window, 'start_sleep_time_seconds_from_midnight')
+        assert hasattr(window, 'end_sleep_time_seconds_from_midnight')
